d7d8030805af57aca2942cdce7d3e576e0ad6c62
Muhammad Rizky Ardiansah <rizkyardiansah.dev@gmail.com>
2025-03-10 20:48:00 +0700

    Enhance Navbar component with Chakra UI for improved layout and styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,31 @@
+import { Container, Flex, Link, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container maxW={"1140px"} px={"4"} my={"5"}>
+      <Flex
+        h={"16px"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          lg: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
+      </Flex>
+    </Container>
+  );
 };
 
 export default Navbar;
